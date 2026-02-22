52a5eca39ab9231eadd837b94257b895eb6b4890
Jacob Pratley <pratleyjacob@gmail.com>
2026-02-22 18:16:45 -0500

    Last typos I hope

--- Platforms/WASM/Gamepad/HIDraGamepad_WASM.h
+++ Platforms/WASM/Gamepad/HIDraGamepad_WASM.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "../../../HIDraDefs.h"
+#include "../../../Defs/HIDraDefs.h"
 
 #include "../../../Gamepad/HIDraGamepad.h"
 
 #include <string>
 #include <vector>
 
-#if (HIDra_Platform == HIDra_WebAssembly) && HIDra_Gamepad
+#if (HIDra_Platform == HIDra_Platform_WebAssembly) && HIDra_Gamepad
 namespace HIDra
 {
     class Gamepad_WASM : public GamepadBase
@@ -14,4 +14,4 @@ namespace HIDra
 
     using Gamepad = Gamepad_WASM;
 } // namespace HIDra
-#endif // (HIDra_Platform == HIDra_WebAssembly) && HIDra_Gamepad
+#endif // (HIDra_Platform == HIDra_Platform_WebAssembly) && HIDra_Gamepad
