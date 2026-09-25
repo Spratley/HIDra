ac04ab0f820825e2d374d15803c3696f3252ff41
Jacob Pratley <pratleyjacob@gmail.com>
2026-09-24 22:28:58 -0400

    Added simple stick deadzones

--- Gamepad/HIDraGamepad.cpp
+++ Gamepad/HIDraGamepad.cpp
@@ -1,7 +1,7 @@
 #include "HIDraGamepad.h"
 #if HIDra_Gamepad
 
-namespace
+namespace HIDraGamepad_Private
 {
     template <typename ValueType>
     inline ValueType AddAveragedValue(ValueType const& previousAverage,
@@ -10,7 +10,25 @@ namespace
     {
         return ((previousAverage * previousCount) + value) / (previousCount + 1);
     }
-} // namespace
+
+    // TODO: Expose as a parameter to be configured?
+    constexpr float DeadZone = 0.05f;
+
+    constexpr float SanitizeFloat(float value) 
+    {
+        // MSVC doesn't have a constexpr std::abs()...
+        if ((value < 0.0f && value > -DeadZone) || (value > 0.0f && value < DeadZone))
+        {
+            return 0.0f;
+        }
+        return value;
+    }
+
+    constexpr HIDra::Vec2f SanitizeStick(HIDra::Vec2f const& stickValue)
+    {
+        return HIDra::Vec2f{ SanitizeFloat(stickValue.m_x), SanitizeFloat(stickValue.m_y) };
+    }
+} // namespace HIDraGamepad_Private
 
 namespace HIDra
 {
@@ -22,6 +40,8 @@ namespace HIDra
               static_cast<GamepadButtonFlags>(m_inputData.m_buttonFlags ^ report.m_buttonFlags);
             m_inputReportsThisFrame++;
             m_inputData = report;
+            m_inputData.m_stickL = HIDraGamepad_Private::SanitizeStick(report.m_stickL);
+            m_inputData.m_stickR = HIDraGamepad_Private::SanitizeStick(report.m_stickR);
             return;
         }
 
@@ -30,10 +50,14 @@ namespace HIDra
           m_buttonFlagsSetThisFrame | (m_inputData.m_buttonFlags ^ report.m_buttonFlags));
         m_inputData.m_buttonFlags = report.m_buttonFlags;
 
-        AddAveragedValue(m_inputData.m_stickL, m_inputReportsThisFrame, report.m_stickL);
-        AddAveragedValue(m_inputData.m_stickR, m_inputReportsThisFrame, report.m_stickR);
-        AddAveragedValue(m_inputData.m_triggerL, m_inputReportsThisFrame, report.m_triggerL);
-        AddAveragedValue(m_inputData.m_triggerR, m_inputReportsThisFrame, report.m_triggerR);
+        HIDraGamepad_Private::AddAveragedValue(m_inputData.m_stickL,
+                                               m_inputReportsThisFrame,
+                                               HIDraGamepad_Private::SanitizeStick(report.m_stickL));
+        HIDraGamepad_Private::AddAveragedValue(m_inputData.m_stickR,
+                                               m_inputReportsThisFrame,
+                                               HIDraGamepad_Private::SanitizeStick(report.m_stickR));
+        HIDraGamepad_Private::AddAveragedValue(m_inputData.m_triggerL, m_inputReportsThisFrame, report.m_triggerL);
+        HIDraGamepad_Private::AddAveragedValue(m_inputData.m_triggerR, m_inputReportsThisFrame, report.m_triggerR);
     }
 } // namespace HIDra
 #endif // HIDra_Gamepad
--- HIDraTypes.h
+++ HIDraTypes.h
@@ -43,8 +43,8 @@ namespace HIDra
     {
         static const Vec2f s_zero;
 
-        Vec2f() = default;
-        Vec2f(float x, float y)
+        constexpr Vec2f() = default;
+        constexpr Vec2f(float x, float y)
             : m_x(x)
             , m_y(y)
         {}
